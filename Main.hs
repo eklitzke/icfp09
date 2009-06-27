f5613b24344253978ca9e289412413f4781ae6d3
Evan Klitzke <evan@eklitzke.org>
2009-06-27 11:15:30 -0700

    support looping forever

--- Main.hs
+++ Main.hs
@@ -1,12 +1,34 @@
 module Main where
 
 import System.Environment (getArgs)
-import TeamCA.Machine (run)
+
+import TeamCA.Machine.Types
+import TeamCA.Machine
+import Data.Map
+
+configurationPort :: Int
+configurationPort = 16000
+
+runSimulator :: FilePath -> Int -> (Ports -> Ports) -> IO ()
+runSimulator fp cfg mut = do
+  world <- readWorld fp
+  run (updateWorld world configure)
+  return ()
+  where
+    run w = do w' <- runWorld w
+               print w'
+               run $ updateWorld w' mut
+
+    configure :: Ports -> Ports
+    configure = insert configurationPort (fromIntegral cfg)
+
+strategy :: Ports -> Ports
+strategy = id
 
 main = do
     print "starting simulator"
     args <- getArgs
     case args of
         [] -> error "expecting a file"
-        x -> mapM run x
-    return ()
+        [obfName, config] -> do let cfg = read config :: Int
+                                runSimulator obfName cfg strategy

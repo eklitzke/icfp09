31f87f56393a2e64ae201f504401b5f37f0c27d5
Evan Klitzke <evan@eklitzke.org>
2009-06-28 14:12:16 -0700

    draw the earth as a blue circle

--- vis/translate.py
+++ vis/translate.py
@@ -13,7 +13,13 @@ if __name__ == '__main__':
 			inited = True
 
 		output = []
-		output.append({'shape': 'circle', 'R': data['oRadius'], 'x': 0, 'y': 0})
+
+		# Draw the target radius as a green circle
+		output.append({'shape': 'circle', 'R': data['oRadius'], 'x': 0, 'y': 0, 'r': 0, 'b': 0, 'g': 1})
+
+		# Draw the satellite as a red dot
 		output.append({'shape': 'dot', 'x': data['oPos'][0], 'y': data['oPos'][1], 'r': 1, 'b': 0, 'g': 0})
-		output.append({'shape': 'dot', 'x': 0, 'y': 0, 'b': 1, 'r': 0, 'g': 0})
+
+		# Draw the earth as a blue dot at (0, 0)
+		output.append({'shape': 'circle', 'R': 6.357e6, 'x': 0, 'y': 0, 'b': 1, 'r': 0, 'g': 0})
 		outfile.write(simplejson.dumps(output) + '\n')
